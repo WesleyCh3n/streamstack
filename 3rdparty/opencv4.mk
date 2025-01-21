ab152720de072e8f3294f3d34633cc3e007a5eb3
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2025-01-21 10:52:44 +0800

    fix: path issue

--- 3rdparty/opencv4.mk
+++ 3rdparty/opencv4.mk
@@ -9,15 +9,15 @@ OPENCV_TAR = opencv.tar.gz
 OPENCV_SRC_DIR = opencv-src
 
 OPENCV_CONTRIB_SRC_URL = https://github.com/opencv/opencv_contrib/archive/refs/tags/$(VERSION).tar.gz
-OPENCV_CONTRIB_TARa = opencv_contrib.tar.gz
-OPENCV_CONTRIB_SRC_DIR = opencv_contrib-src/modules
+OPENCV_CONTRIB_TAR = opencv_contrib.tar.gz
+OPENCV_CONTRIB_SRC_DIR = opencv_contrib-src
 
 all:
-	@curl -L $(OPENCV_SRC_URL) -o $(OPENCV_TAR)
-	@curl -L  $(OPENCV_CONTRIB_SRC_URL) -o $(OPENCV_CONTRIB_TAR)
+	@[ -s $(OPENCV_TAR) ] || curl -L $(OPENCV_SRC_URL) -o $(OPENCV_TAR)
+	@[ -s $(OPENCV_CONTRIB_TAR) ] ||  curl -L  $(OPENCV_CONTRIB_SRC_URL) -o $(OPENCV_CONTRIB_TAR)
 	@mkdir -p $(OPENCV_SRC_DIR) && mkdir -p $(OPENCV_CONTRIB_SRC_DIR)
 	@tar xf $(OPENCV_TAR) --strip-components=1 -C $(OPENCV_SRC_DIR)
-	@tar xf $(OPENCV_CONTRIB_TAR) --strip-components=1 -C $(OPENCV_SRC_DIR)
+	@tar xf $(OPENCV_CONTRIB_TAR) --strip-components=1 -C $(OPENCV_CONTRIB_SRC_DIR)
 	@cmake \
 		-DCMAKE_BUILD_TYPE=$(BUILD_TYPE) \
 		-DCMAKE_INSTALL_PREFIX=$(INSTALL_PREFIX) \
@@ -28,11 +28,12 @@ all:
 		-DOPENCV_ENABLE_NONFREE=ON \
 		-DWITH_JPEG=ON -DBUILD_JPEG=ON \
 		-DWITH_FFMPEG=ON -DVIDEOIO_PLUGIN_LIST=ffmpeg \
-		-DOPENCV_EXTRA_MODULES_PATH=$(OPENCV_CONTRIB_SRC_DIR) \
-		-DBUILD_LIST=core,dnn,videoio,cudev \
+		-DOPENCV_EXTRA_MODULES_PATH=$(OPENCV_CONTRIB_SRC_DIR)/modules \
+		-DBUILD_LIST=core,dnn,videoio,cudev,cudacodec \
 		-B $(BUILD_DIR) \
 		$(OPENCV_SRC_DIR)
-	cmake --build $(BUILD_DIR) -- -j$(shell expr $(nproc --all) / 2)
+	@echo "cmake --build $(BUILD_DIR) -- -j$$(($(shell nproc --all) / 2))"
+	@sudo cmake --install $(BUILD_DIR)
 
 help:
 	cmake \
